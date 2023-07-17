1abd20c877b36d13e44d473a9dadae551ef49caf
Min Cheol Kim <kmc724@zinggls.com>
2023-07-17 14:10:22 +0900

    Sing song.mp3 using ffmpeg

--- iron_bot.py
+++ iron_bot.py
@@ -1,5 +1,6 @@
 import discord
 import iron_token
+import time
 
 TOKEN = iron_token.TOKEN
 CHANNEL_ID = iron_token.CHANNEL_ID
@@ -13,11 +14,12 @@ class Client(discord.Client):
     async def on_message(self, message):
         if (message.author == self.user):
             return;
-        ch = self.get_channel(message.author.voice.channel.id)
-        await ch.connect()
         await self.channel.send(message.content)
 
+        ch = self.get_channel(message.author.voice.channel.id)
+        vc = await ch.connect()
 
+        vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=iron_token.SONG))
 
 intents = discord.Intents.default()
 intents.message_content = True
