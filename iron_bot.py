aba03d555ab716ee7958791072d43eb711e17570
Min Cheol Kim <kmc724@zinggls.com>
2023-07-17 11:20:59 +0900

    Echo message

--- iron_bot.py
+++ iron_bot.py
@@ -5,8 +5,13 @@ CHANNEL_ID = 1130318451249008784
 
 class Client(discord.Client):
     async def on_ready(self):
-        channel = self.get_channel(CHANNEL_ID)
-        await channel.send("Hello World")
+        self.channel = self.get_channel(CHANNEL_ID)
+        await self.channel.send("Hello World")
+
+    async def on_message(self, message):
+        if (message.author == self.user):
+            return;
+        await self.channel.send(message.content)
 
 intents = discord.Intents.default()
 intents.message_content = True
