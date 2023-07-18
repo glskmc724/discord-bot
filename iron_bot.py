dee9d5504a4606ef9d1d3e6dfb664ca243172c14
Min Cheol Kim <kmc724@zinggls.com>
2023-07-18 11:39:40 +0900

    Verify channel id from channel.lists, unverified channel can not receive message

--- iron_bot.py
+++ iron_bot.py
@@ -4,29 +4,42 @@ import asyncio
 import youtube
 
 TOKEN = iron_token.TOKEN
-CHANNEL_ID = iron_token.CHANNEL_ID
+
+def verify_channel(channel_id):
+    filename = "channel.lists"
+    channel_list_file = open(filename, 'r')
+    channel_list = channel_list_file.readlines()
+
+    for channel in channel_list:
+        if (channel[0] == '#'):
+            continue
+        elif (int(channel) == channel_id):
+            return True
+    return False
 
 class Client(discord.Client):
     async def on_ready(self):
-        self.channel = self.get_channel(CHANNEL_ID)
-        await self.channel.send("Hello World")
+        return;
 
     async def on_message(self, message):
         if (message.author == self.user):
             return;
 
-        if (message.channel.id != CHANNEL_ID):
+        if (verify_channel(message.channel.id) != True):
             return;
-        
-        #await self.channel.send(message.content)
 
-        ch = self.get_channel(message.author.voice.channel.id)
-        vc = await ch.connect()
+        #channel = self.get_channel(message.channel.id)
+        #await channel.send(message.content)
         
+        # find music
         res = youtube.search(message.content)
         link = res[0]['href']
-
         song = youtube.download(link)
+
+        # connect
+        ch = self.get_channel(message.author.voice.channel.id)
+        vc = await ch.connect()
+
         vc.play(discord.FFmpegPCMAudio(executable = "ffmpeg", source = song))
 
         while vc.is_playing():
