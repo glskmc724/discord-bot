d840816ad993129f59a9a426f45c5859bbd3e5e5
Min Cheol Kim <kmc724@zinggls.com>
2023-07-18 18:06:12 +0900

    Add button ui, and bot can delete message. Error you pause music, modify will tomorror

--- iron_bot.py
+++ iron_bot.py
@@ -3,6 +3,9 @@ import iron_token
 import asyncio
 import youtube
 
+from discord.ui import Button, View
+from discord import ButtonStyle
+
 TOKEN = iron_token.TOKEN
 
 def verify_channel(channel_id):
@@ -28,13 +31,26 @@ class Client(discord.Client):
         if (verify_channel(message.channel.id) != True):
             return;
 
-        #channel = self.get_channel(message.channel.id)
+        channel = self.get_channel(message.channel.id)
         #await channel.send(message.content)
         
+        msg = [message]
+
         # find music
         res = youtube.search(message.content)
         link = res[0]['href']
         song = youtube.download(link)
+
+        async def pause_callback(interaction):
+            if (vc.is_playing()):
+                vc.stop()
+            msg.append(await interaction.response.send_message("Pause music"))
+
+        pause_btn = Button(label = "Pause", style = ButtonStyle.primary)
+        pause_btn.callback = pause_callback
+        view = View()
+        view.add_item(pause_btn)
+        msg.append(await channel.send("Play Music...", view = view))
 
         # connect
         ch = self.get_channel(message.author.voice.channel.id)
@@ -46,6 +62,8 @@ class Client(discord.Client):
             await asyncio.sleep(0.1)
         
         await vc.disconnect()
+        for i in msg:
+            await i.delete()
 
 intents = discord.Intents.default()
 intents.message_content = True
